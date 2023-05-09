98a8566f1505260d86ac7a2f9d5aa7a75948064d
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-05-09 22:34:27 +0500

    first commit :)

---
+++ src/components/MenuItems.jsx
@@ -0,0 +1,51 @@
+import Image1 from '../assets/images/card-img-1.jpg'
+import Image2 from '../assets/images/card-img-2.jpg'
+import Image3 from '../assets/images/card-img-3.jpg'
+import Image4 from '../assets/images/card-img-4.jpg'
+import Image5 from '../assets/images/card-img-5.jpg'
+import Image6 from '../assets/images/card-img-6.jpg'
+
+export const MenuItems = [
+    {
+        img: Image1,
+        title: "Aktivlar Zali",
+        about: "Bu zalda maktabdagi har xil bayramlar bo'lib o'tadi.",
+        view: 56,
+        linkes: 27
+    },
+    {
+        img: Image2,
+        title: "Aktivlar Zali",
+        about: "Bu zalda maktabdagi har xil bayramlar bo'lib o'tadi.",
+        view: 34,
+        linkes: 14
+    },
+    {
+        img: Image3,
+        title: "Aktivlar Zali",
+        about: "Bu zalda maktabdagi har xil bayramlar bo'lib o'tadi.",
+        view: 45,
+        linkes: 31
+    },
+    {
+        img: Image4,
+        title: "Aktivlar Zali",
+        about: "Bu zalda maktabdagi har xil bayramlar bo'lib o'tadi.",
+        view: 32,
+        linkes: 12
+    },
+    {
+        img: Image5,
+        title: "Aktivlar Zali",
+        about: "Bu zalda maktabdagi har xil bayramlar bo'lib o'tadi.",
+        view: 67,
+        linkes: 45
+    },
+    {
+        img: Image6,
+        title: "Aktivlar Zali",
+        about: "Bu zalda maktabdagi har xil bayramlar bo'lib o'tadi.",
+        view: 63,
+        linkes: 51
+    },
+]
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './Home.scss'
+import "./Home.scss";
+import { MenuItems } from '../MenuItems'
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="home-title">
-        <h1>To`rtko`l tumani 10-sonli maktab</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Velit asperiores quas obcaecati sint aliquid
-          ad quo natus.
-        </p>
-        <Link to='/about'>
-          <i class="fa-solid fa-circle-info"></i> Biz haqqimizda
-        </Link>
-        <Link to='/contact'>
-          <i class="fa-solid fa-address-book"></i> Aloqa
-        </Link>
+    <>
+      <div className="home">
+        <div className="home-title">
+          <h1>To`rtko`l tumani 10-sonli maktab</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+            asperiores quas obcaecati sint aliquid ad quo natus.
+          </p>
+          <Link to="/about">
+            <i class="fa-solid fa-circle-info"></i> Biz haqqimizda
+          </Link>
+          <Link to="/contact">
+            <i class="fa-solid fa-address-book"></i> Aloqa
+          </Link>
+        </div>
       </div>
-    </div>
-  )
+      <div className="home-items">
+        <div className="cards">
+          {MenuItems.map((item, index) => {
+            return (
+              <div className="card" key={index}>
+                <div className="card-head">
+                  <img src={item.img} alt="" />
+                </div>
+                <div className="card-body">
+                  <h1>{item.title}</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Incidunt, mollitia.
+                  </p>
+                  <a href="">See more</a>
+                </div>
+                <div className="card-end">
+                  <span>
+                    <i class="fa-solid fa-eye"></i> {item.view}
+                  </span>
+                  <span>❤️ {item.linkes}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
