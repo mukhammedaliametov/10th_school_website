340b79edf079b9c9dcd855c603c65c53bbcb5476
Mukhammedali Ametov <ametovmukhammedali07@gmail.com>
2023-05-12 18:01:31 +0500

    first commit

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import SectionTitle from "../SectionTitle";
-import Maps from "../Maps";
 
 const Contact = () => {
   return(
     <div className="contact">
       <SectionTitle sectionName="Aloqa" />
       <div className="location">
-        <Maps />
+      
       </div>
     </div>
   ) 
